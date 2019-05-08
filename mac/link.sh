60850e049f92c53f74a649021f80a0748e6c8ae4
windrammer <david.tollman@izettle.com>
2019-05-08 15:10:44 +0200

    Updated link script to not break zsh

--- mac/link.sh
+++ mac/link.sh
@@ -2,14 +2,23 @@
 DATE=$(date +"%Y%m%d%H%M")
 here=$(pwd)
 
-mv ~/.vimrc $here/oldfiles/vimrc-$DATE
-ln -s $here/vimrc ~/.vimrc
+# mv ~/.vimrc $here/oldfiles/vimrc-$DATE
+# ln -s $here/vimrc ~/.vimrc
+
+mv ~/.vimrc /Users/david.tollman/dotfiles/mac/oldfiles/vimrc-$DATE
+ln -s /Users/david.tollman/dotfiles/mac/vimrc ~/.vimrc
+
+# rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
+# ln -s $here/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
 rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
-ln -s $here/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
+ln -s /Users/david.tollman/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
+
+# rm ~/.zshrc
+# ln -s $here/zshrc ~/.zshrc
 
 rm ~/.zshrc
-ln -s $here/zshrc ~/.zshrc
+ln -s /Users/david.tollman/dotfiles/mac/zshrc ~/.zshrc
 
 # touch ~/.Xmodmap
 # echo 'clear Lock' >> ~/.Xmodmap
