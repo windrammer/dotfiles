33ff04b2cab2db3392e7e9af92fc0234326450a1
David Tollman <david.tollman@kambi.com>
2022-09-06 09:21:21 +0200

    update p10k stuff

--- mac/link.sh
+++ mac/link.sh
@@ -11,9 +11,11 @@ ln -s /Users/davtol/git/dotfiles/mac/vimrc ~/.vimrc
 # rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
 # ln -s $here/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
-rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
-ln -s /Users/davtol/git/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
+# rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
+# ln -s /Users/davtol/git/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
+rm ~/.p10k.zsh
+ln -s /Users/davtol/git/dotfiles/mac/p10k.zsh ~/.p10k.zsh
 # rm ~/.zshrc
 # ln -s $here/zshrc ~/.zshrc
 
