05945e7051a323d1eea9130760585d29832ee4bd
David Tollman <mail@toll.is>
2025-03-25 23:25:54 +0100

    update link script

--- mac/link.sh
+++ mac/link.sh
@@ -5,27 +5,25 @@ here=$(pwd)
 # mv ~/.vimrc $here/oldfiles/vimrc-$DATE
 # ln -s $here/vimrc ~/.vimrc
 
-mv ~/.vimrc /Users/david/git/dotfiles/mac/oldfiles/vimrc-$DATE
-ln -s /Users/david/git/dotfiles/mac/vimrc ~/.vimrc
+mv ~/.vimrc /Users/$USER/git/dotfiles/mac/oldfiles/vimrc-$DATE
+ln -s /Users/$USER/git/dotfiles/mac/vimrc ~/.vimrc
 
 # rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
 # ln -s $here/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
 
-<<<<<<< Updated upstream
-# rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
-# ln -s /Users/davtol/git/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
-=======
 rm ~/.oh-my-zsh/themes/agnoster.zsh-theme
-ln -s /Users/david/git/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
->>>>>>> Stashed changes
+ln -s /Users/$USER/git/dotfiles/mac/agnoster.zsh-theme ~/.oh-my-zsh/themes/agnoster.zsh-theme
+
+rm ~/Brewfile
+ln -s /Users/$USER/git/dotfiles/mac/Brewfile ~/Brewfile
 
 rm ~/.p10k.zsh
-ln -s /Users/davtol/git/dotfiles/mac/p10k.zsh ~/.p10k.zsh
+ln -s /Users/$USER/git/dotfiles/mac/p10k.zsh ~/.p10k.zsh
 # rm ~/.zshrc
 # ln -s $here/zshrc ~/.zshrc
 
 rm ~/.zshrc
-ln -s /Users/david/git/dotfiles/mac/zshrc ~/.zshrc
+ln -s /Users/$USER/git/dotfiles/mac/zshrc ~/.zshrc
 
 # touch ~/.Xmodmap
 # echo 'clear Lock' >> ~/.Xmodmap
