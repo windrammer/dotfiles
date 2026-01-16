e2a06478f66b58147f6b671466d160cb1397a076
David Tollman <david.tollman@playgroundtech.io>
2026-01-16 14:49:04 +0100

    mega fix

--- mac/link.sh
+++ mac/link.sh
@@ -25,6 +25,16 @@ ln -s /Users/$USER/git/dotfiles/mac/p10k.zsh ~/.p10k.zsh
 rm ~/.zshrc
 ln -s /Users/$USER/git/dotfiles/mac/zshrc ~/.zshrc
 
+mkdir -p ~/.git-hooks
+rm -f ~/.git-hooks/pre-commit
+ln -s /Users/$USER/git/dotfiles/mac/git-hooks/pre-commit ~/.git-hooks/pre-commit
+
+rm -f ~/Library/Preferences/com.googlecode.iterm2.plist
+ln -s /Users/$USER/git/dotfiles/mac/iterm2-profile.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+
+rm ~/Library/Preferences/com.knollsoft.Rectangle.plist
+ln -s /Users/$USER/git/dotfiles/mac/rectangle-config.json ~/Library/Preferences/com.knollsoft.Rectangle.plist
+
 # touch ~/.Xmodmap
 # echo 'clear Lock' >> ~/.Xmodmap
 # echo 'keycode 66 = Escape NoSymbol Escape' >> ~/.Xmodmap
